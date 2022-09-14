8c5b91177151ab7248e79bb880178d2993e853ef
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-14 19:59:10 +0530

    created database Car Sales

---
+++ src/main/resources/car_sales_data_queries.sql
@@ -0,0 +1,7 @@
+-- create database CarSales
+CREATE DATABASE IF NOT EXISTS `CarSales`;
+
+-- Create a table sales person in Car sales database
+-- added sales_id, FirstName, City, Commission rate as columns
+--added sales_id as primary key
+
