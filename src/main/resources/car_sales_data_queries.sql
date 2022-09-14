5a7c8903a5610f05d24c02444fe4a2cb1dfceff3
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-14 20:03:08 +0530

    insert the details of each sales person

--- src/main/resources/car_sales_data_queries.sql
+++ src/main/resources/car_sales_data_queries.sql
@@ -11,3 +11,16 @@ CREATE TABLE IF NOT EXISTS `Salesperson` (
     city varchar(10),
     commision_rate int
 );
+---Insert the details of sales person john
+SELECT * FROM salesperson;
+-- insert the details of the sales person john
+INSERT INTO `CarSales`.`salesperson` VALUES('1001','John','London','12');
+--insert the details of the sales person Bob
+INSERT INTO `CarSales`.`salesperson` VALUES('1002', 'Bob','SanJose','13');
+-- insert the details of the sales person Judith
+INSERT INTO `CarSales`.`salesperson` VALUES('1004', 'Judith','London','11');
+
+INSERT INTO `CarSales`.`salesperson` VALUES('1007', 'Robbin','Barcelona','15');
+INSERT INTO `CarSales`.`salesperson` VALUES('1003', 'Harry','NewYork','10');
+INSERT INTO `CarSales`.`salesperson` VALUES('1005', 'James','London','26');
+INSERT INTO `CarSales`.`salesperson` VALUES('1008', 'Charles','Florida','0');
